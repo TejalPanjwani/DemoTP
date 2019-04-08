7d99ba139d60bbaf4410a20ea0d47c82edfd9ebe
tejal <tejal.panjwani@bacancytechnology.com>
2019-04-08 12:15:48 +0530

    create sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end 
+
+    def create
+        @user = User.new(user_pramas)
+        if @user.save
+            # puts "=============================================="
+            # puts @user.attributes.to_yaml
+            # puts "============================================="
+            # puts  @user.attributes
+            # render :json=> @user
+            flash[:success] = "Welcome to the Sample App!"
+            redirect_to  users_path(@user)
+
+        else
+            render 'new'
+        end
+    end 
+
+    def show
+        @user = User.find(params[:id]) 
+    end 
+
+
+    private
+    def user_pramas
+        params.require(:users).permit(:name,:email,:password,:password_confirmation)
+    end 
+
+
 end
